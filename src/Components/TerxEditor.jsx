f8b927208f660b83b953b4ab42af58284ca5794f
Harshul Jain <harshulgangwal02@gmail.com>
2023-05-21 10:48:11 +0530

    Added Text Editor

--- src/Components/TerxEditor.jsx
+++ src/Components/TerxEditor.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Editor } from "react-draft-wysiwyg";
+import { EditorState } from "draft-js";
+
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
 function TerxEditor() {
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+
+  const onEditorStateChange = (editorState) => {
+    setEditorState(editorState);
+  }
+
   return (
-    <div>
-      <h1>Text Editor</h1>
+    <div className='bg-[#F8F9FA] min-h-screen pb-16'>
+      <div className="flex">
+        <div className="flex-grow">
+          <Editor
+            editorState={editorState}
+            toolbarClassName="flex sticky top-0 z-50"
+            wrapperClassName="wrapperClassName"
+            editorClassName="mt-6 p-10 bg-white shadow-lg max-w-5xl mx-auto mb-12 border"
+            onEditorStateChange={onEditorStateChange}
+          />
+        </div>
+        <div className="w-14 bg-gray-200 h-screen"> {/* Sidebar */}
+          {/* Sidebar content */}
+          <ul className="py-4">
+            <li className="flex items-center p-2 hover:bg-gray-300">
+              <span className="material-icons mr-2"></span> 
+            </li>
+            <li className="flex items-center p-2 hover:bg-gray-300">
+              <span className="material-icons mr-2"></span>
+            </li>
+            <li className="flex items-center p-2 hover:bg-gray-300">
+              <span className="material-icons mr-2"></span> 
+            </li>
+            {/* Add more Google apps as required */}
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default TerxEditor
+export default TerxEditor;
